f79181ed6856d42db6165d9195a96e63f84e3436
tray0019 <tray0019@algonquinlive.com>
2024-02-15 16:20:25 -0500

    Rustom Trayvilla: FINISHH!! FIRST WEEK

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -3,6 +3,7 @@ package com.college.converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +22,10 @@ import android.widget.TextView;
     ** Each task must be done by a different team member.
 
 */
-
+/*
+Rustom: Add **Log** messages at the entry/exit of **onCreate()** and
+**convertCurrency()** methods. Level should be **Info** //Rustom
+ */
 public class MainActivity extends AppCompatActivity {
     static private final Float CONVERSION_RATE = 0.80F;
     @Override
@@ -29,8 +33,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Log entry message for onCreate()
+        Log.i("MainActivity","onCreate() method started");
         Button buttonConvert = findViewById(R.id.convertButton);
 
+        Log.i("MainActivity","onCreate() method finished");
         buttonConvert.setOnClickListener( view ->  {
             convertCurrency(view);
         } );
@@ -38,11 +45,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void convertCurrency(View view) {
 
+        Log.i("MainActivity","convertCurrency() method started");
         EditText inputView = findViewById(R.id.entryId);
-
         String inputAmount = inputView.getText().toString();
-
         TextView resultView = findViewById(R.id.resultId);
+
 
         if (!inputAmount.isEmpty()) {
             Float inputAmountDecimal = Float.valueOf(inputAmount);
@@ -50,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
             Float resultFloat = inputAmountDecimal * CONVERSION_RATE;
 
             resultView.setText( resultFloat + " Euros" );
+            Log.i("MainActivity","convertCurrency() method finished");
         }
     }
 }
+
